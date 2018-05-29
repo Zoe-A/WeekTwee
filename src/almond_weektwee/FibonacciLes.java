f545b1ad830485b0c73ff8e95d7a144b77042e08
Zoe-A <zoe.almond.ah@gmail.com>
2018-05-29 15:54:53 +0200

    Einde les 29/05

---
+++ src/almond_weektwee/FibonacciLes.java
@@ -0,0 +1,21 @@
+package almond_weektwee;
+
+public class FibonacciLes {
+    public static void main(String[] args) {
+
+        int n = 30;
+        int a = 0;
+        int b = 1;
+
+        System.out.print(a + " " + b + " ");
+
+        for (int i = 0; i <= n; i++) {
+            int som = a + b;
+            System.out.print(som + " ");
+            a = b;
+            b = som;
+        }
+
+
+    }
+}
---
+++ src/almond_weektwee/Loop.java
@@ -0,0 +1,19 @@
+package almond_weektwee;
+
+public class Loop {
+    public static void main(String[] args) {
+       /* int i = 1;
+        int totaal = 0;
+        while (i <= 100) {
+            totaal = totaal + i;
+            i = i + 1;
+        }
+        System.out.println(totaal);*/
+
+        int total = 0;
+        for (int i = 1; i <= 100; i = i++) {
+            total +=i;
+            }
+        System.out.println(total);
+    }
+}
