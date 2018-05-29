dc701d5452e92fd2ac87e3d31f0fe268cb0a00b8
vdab cursist <zoe.almond.ah@gmail.com>
2018-05-29 14:04:09 +0200

    Week twee

---
+++ src/almond_weektwee/FunctiesWT.java
@@ -0,0 +1,39 @@
+package almond_weektwee;
+
+public class FunctiesWT {
+    public static void main(String[] args) {
+
+        double a ;
+        double r = 105;
+        a = Math.PI*Math.pow(r,(double)2);
+        System.out.println(a);
+
+        double c ;
+        double twee = 2;
+        c = 2*Math.pow((Math.PI),twee);
+        System.out.println(c);
+
+        double d;
+        double x = 5;
+        d = (twee * x) + Math.sin(x/3);
+        System.out.println(d);
+
+        double e;
+        double n = 4;
+        e = (Math.log10(twee)*n) / Math.sqrt(n);
+        System.out.println(e);
+
+        double f;
+        f = r + (3 * Math.sqrt(x)) + twee;
+        System.out.println(f);
+
+        double h;
+        double g = 6.67e-11;
+        double m1 = 3;
+        double m2 = 9;
+        h = g * ((m1 * m2)/r);
+        System.out.println(h);
+
+
+    }
+}
---
+++ src/almond_weektwee/RandomNumber.java
@@ -0,0 +1,15 @@
+package almond_weektwee;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        int max = 10;
+        int min = 5;
+        int r = min + (int) (Math.random() * (max - min));
+        System.out.println(r);
+
+
+
+
+
+    }
+}
